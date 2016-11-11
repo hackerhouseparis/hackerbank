f7c344afa97e8d181c4d7d577286b00b17f7981e
GuillaumeBadi <gbadi@student.42.fr>
2016-11-11 18:56:37 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
+
+  resolve: {
+    root: path.resolve('./src'),
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js']
+  },
+
   module: {
+
     loaders: [{
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
-};
+      include: path.join(__dirname, 'src'),
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass'],
+      include: path.join(__dirname, 'src'),
+    }],
+  },
+}
